27e2fdd4cf72771e6bc1d9a60c9458df38f07ba2
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 13:22:44 -0500

    miuicamera: Remove elf files in copy files check
    New extract files uses packages instead of copy files
    
    Change-Id: I337c07d13bd350958d4808e1693cc6e03753a96c

--- BoardConfigMiuiCamera.mk
+++ BoardConfigMiuiCamera.mk
@@ -12,7 +12,6 @@ MIUICAMERA_PATH := vendor/xiaomi/miuicamera
 # Build Hacks
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_DUP_SYSPROP := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
