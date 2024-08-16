12ba6922d100dc27d8e421c9b0d742bbb30230a7
COSMIC <rayavanindra@gmail.com>
2024-08-16 20:51:13 +0530

    miuicamera: Add required system props

--- BoardConfigMiuiCamera.mk
+++ BoardConfigMiuiCamera.mk
@@ -6,3 +6,8 @@
 
 # Inherit from the proprietary version
 include vendor/xiaomi/miuicamera/common/BoardConfigVendor.mk
+
+MIUICAMERA_PATH := vendor/xiaomi/miuicamera
+
+# Properties
+TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
