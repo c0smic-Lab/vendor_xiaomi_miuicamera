5b1bf2c0150852ecab8dc3e7b320402e94731add
COSMIC <rayavanindra@gmail.com>
2024-08-16 20:41:24 +0530

    miuicamera: Import required configs and permission
    * From stock

--- MiuiCamera.mk
+++ MiuiCamera.mk
@@ -7,6 +7,14 @@
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/miuicamera/common/common-vendor.mk)
 
+# Priv-app permission
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-miui.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miui.xml
+
 # Shims
 PRODUCT_PACKAGES += \
     libgui_shim_miuicamera
+
+# Sysconfig
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml
