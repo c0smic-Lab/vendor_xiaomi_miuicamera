bea5631783cfa6ee7124494d477427ea4746cf40
COSMIC <rayavanindra@gmail.com>
2024-08-16 18:03:53 +0530

    miuicamera: Initial commit

---
+++ BoardConfigMiuiCamera.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+include vendor/xiaomi/miuicamera/common/BoardConfigVendor.mk
